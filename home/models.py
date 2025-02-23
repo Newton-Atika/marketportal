e6080d976956e76737560ff5cd8e9966314db5fa
Newton-Atika <163111440+Newton-Atika@users.noreply.github.com>
2025-02-23 11:42:13 +0300

    Update models.py

--- home/models.py
+++ home/models.py
@@ -15,10 +15,10 @@ class Product(models.Model):
 
 
 class ShoppingList(models.Model):
+    session_key = models.CharField(max_length=40, blank=True, null=True)  # Unique per visitor
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    unique_id = models.UUIDField(default=uuid.uuid4,editable=False,unique=True)
-    picked = models.BooleanField(default=False) 
 
     def __str__(self):
-        return f'Shopping List {self.unique_id}'
+        return f"{self.product.name} - {self.quantity}"
